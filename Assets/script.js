b87851f69334353363aa29e6484dc6a1c35ae766
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-06-16 20:42:12 -0500

    implemented basic js alg for pushing questions 6.17.20

---
+++ Assets/script.js
@@ -0,0 +1,38 @@
+//start button of game
+var startBtn = document.getElementById("start-game")
+
+//Event Listener for startBtn
+startBtn.addEventListener("click", console.log("hello"));
+
+//Question function that runs while the timer is greater than 0
+function Questions () {
+    var qbank=["1","2"];
+    
+    while (timer > 0)
+//nested switch within a loop that cycles between questions that displays new questions 
+    for  ( i = 0; i < q){ ii++
+
+        switch (i>=0) {
+            case 1:
+            q = 1
+            var qadd = document.quereyselect("questionDiv")
+            pushQ = qadd.createElement("div")
+            pushQ.textcontent = qbank[1]
+        
+            break;
+
+        case 2: 
+            q = 2
+            var qadd = document.quereyselect("questionDiv")
+            pushQ = qadd.createElement("div")
+            pushQ.textcontent = qbank[1]
+            
+            break;
+        
+    }
+
+}
+
+function timer (){
+
+}
